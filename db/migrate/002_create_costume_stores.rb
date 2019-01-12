d3c0cd4cfe15bee877a7d08b6b99c5464fde35e9
Elise Phan <aliceconnic@gmail.com>
2019-01-12 22:46:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumesStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes_stores do |t|
+      t.string :name
+      t.numeric :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end

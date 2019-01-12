482672a17828d5468cf27be4b4b223ac85d5a33f
Elise Phan <aliceconnic@gmail.com>
2019-01-12 22:57:15 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :num_of_employes
+      t.integer :num_of_employees
       t.boolean :opening_time
       t.boolean :closing_time
     end

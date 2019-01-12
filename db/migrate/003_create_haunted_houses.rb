4b6722deca3542263db3c4d0fdf1c8d41f978d1d
Elise Phan <aliceconnic@gmail.com>
2019-01-12 23:13:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.numeric :num_of_employees
+      t.timestamps
+    end
+  end
+end
